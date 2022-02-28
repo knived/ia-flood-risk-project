dd4540e87f7c9feadbedbc2397ffba71dbd2ccf8
Joseph Lim <jchl9@cam.ac.uk>
2022-02-28 21:29:43 +0000

    Task 2G complete

--- Task2G.py
+++ Task2G.py
@@ -1,19 +1,67 @@
-from floodsystem.stationdata import build_station_list
+import datetime
+from distutils.command.build import build
+from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.flood import stations_highest_rel_level
+from floodsystem.analysis import polyfit
+import numpy as np
+from matplotlib.dates import date2num
 
 def run():
     """Requirements for Task 2G"""
 
     # Build a list of stations
+    stations = build_station_list()
 
     # Update water levels 
-     
-    # Determine whether the water level at a station is rising or falling
+    update_water_levels(stations)
 
-    # Use this to create criteria for risk of flooding
+    # 100 stations with highest level 
+    stations = stations_highest_rel_level(stations, 100)
+    
+    # empty lists for severity classes
+    low=[]
+    moderate=[]
+    high=[]
+    severe=[]
 
-    # List the towns where the risk of flooding is greatest
+    # List for dates and levels and use polyfit for predictions 
+    dt = 10
+    
+    for station in stations:
+        # ignore stations with errors
+        try:
+            dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+            poly, time_shift = polyfit(dates, levels, 4)
+            poly = np.poly1d(poly)
+        except:
+            print("There is an error with {}".format(station.name))
+            continue
+        
+        # use predictions to calculate risks
+        if station.typical_range == None:
+            continue
+        water_level_range = station.typical_range[1] - station.typical_range[0]
+        level_day_after = poly(1)
+        rel_level_day_after = (level_day_after - station.typical_range[0])/water_level_range
 
-    # Rate the risk at ‘severe’, ‘high’, ‘moderate’ or ‘low’
+        # Rate the risk at ‘severe’, ‘high’, ‘moderate’ or ‘low’
+        if rel_level_day_after <= 0.5:
+            low.append(station.name)
+        elif rel_level_day_after <= 1.0:
+            moderate.append(station.name)
+        elif rel_level_day_after <= 2.0:
+            high.append(station.name)
+        else:
+            severe.append(station.name)
+
+    # print number of stations in each category
+    print("The number of stations at low risk are {}".format(len(low)))
+    print("The number of stations at moderate risk are {}".format(len(moderate)))
+    print("The number of stations at high risk are {}".format(len(high)))
+    print("The number of stations at severe risk are {}".format(len(severe)))
+
+        
 
 
 if __name__ == "__main__":
